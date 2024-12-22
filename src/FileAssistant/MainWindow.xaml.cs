d606e2e8bec98cd6c2d751b93a1096b95af240b0
RJ Hollberg <rj.hollberg@outlook.com>
2024-12-22 15:22:56 -0700

    build: add empty WPF application

---
+++ src/FileAssistant/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+using System.Windows;
+
+namespace FileAssistant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
